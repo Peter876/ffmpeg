3903aa86384a99c6ccd297e7b2bec8ffe8f0dafa
Peter876 <piatro97@gmail.com>
2026-04-01 09:23:53 +0300

    An attempt to fix the compilation for X86 Windows

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ require ndk
 import patch-opus.sh
 import libraries.properties
 import libraries.properties from "github.com/pytgcalls/mesa"
-import meson from python3
-import ninja from python3
 
 sysroot_dir="$BUILD_KIT_DIR/usr"
 selected_windows_arch="${FFMPEG_WINDOWS_ARCH:-}"
@@ -26,226 +24,96 @@ fi
 arch_builds=("default")
 
 if is_android; then
-  arch_builds=(
-    "x86_64"
-    "x86"
-    "arm64-v8a"
-    "armv7-a"
-  )
+  arch_builds=("x86_64" "x86" "arm64-v8a" "armv7-a")
 fi
 
 if is_windows; then
-  # Build a single Windows target per run to avoid leaking MSVC state across
-  # architectures during sequential x86_64/x86 builds.
   arch_builds=("x86_64")
-
   if [[ -n "$selected_windows_arch" ]]; then
     case "$selected_windows_arch" in
-      x86|x86_64)
-        arch_builds=("$selected_windows_arch")
-        ;;
-      *)
-        echo "Unsupported FFMPEG_WINDOWS_ARCH: $selected_windows_arch" >&2
-        exit 1
-        ;;
+      x86|x86_64) arch_builds=("$selected_windows_arch") ;;
+      *) echo "Unsupported FFMPEG_WINDOWS_ARCH: $selected_windows_arch" >&2; exit 1 ;;
     esac
   fi
 fi
 
 for arch in "${arch_builds[@]}"; do
   if is_windows; then
+    # Adjust MSVC environment for x86. build-toolkit defaults to x64.
     if [[ "$arch" == "x86" ]]; then
-      opus_prefix="$sysroot_dir/x86"
-      build_and_install "opus" cmake -G "Visual Studio 17 2022" -A Win32 -DCMAKE_INSTALL_PREFIX="$opus_prefix"
-      export PKG_CONFIG_PATH="$opus_prefix/lib/pkgconfig"
-      build_and_install "FFmpeg" configure-static \
-        --windows="--target-os=win32 \
-            --arch=x86 \
-            --toolchain=msvc \
-            --extra-cflags=-I\"$opus_prefix/include\" \
-            --extra-ldflags=-L\"$opus_prefix/lib\"" \
-        --disable-programs --disable-doc \
-        --disable-network --disable-everything \
-        --enable-runtime-cpudetect --enable-protocol=file \
-        --enable-hwaccels --disable-dxva2 \
-        --enable-libopus \
-        --enable-decoder=h264 \
-        --enable-decoder=mp3 \
-        --enable-decoder=mp3adu \
-        --enable-decoder=mp3adufloat \
-        --enable-decoder=mp3float \
-        --enable-decoder=mp3on4 \
-        --enable-decoder=mp3on4float \
-        --enable-decoder=mp1 \
-        --enable-decoder=mp1float \
-        --enable-decoder=mp2 \
-        --enable-decoder=mp2float \
-        --enable-decoder=mp3 \
-        --enable-decoder=mp3adu \
-        --enable-decoder=mp3adufloat \
-        --enable-decoder=mp3float \
-        --enable-decoder=mp3on4 \
-        --enable-decoder=mp3on4float \
-        --enable-decoder=mpeg4 \
-        --enable-decoder=hevc \
-        --enable-decoder=msmpeg4v2 \
-        --enable-decoder=msmpeg4v3 \
-        --enable-decoder=opus \
-        --enable-decoder=pcm_alaw \
-        --enable-decoder=pcm_f32be \
-        --enable-decoder=pcm_f32le \
-        --enable-decoder=pcm_f64be \
-        --enable-decoder=pcm_f64le \
-        --enable-decoder=pcm_lxf \
-        --enable-decoder=pcm_mulaw \
-        --enable-decoder=pcm_s16be \
-        --enable-decoder=pcm_s16be_planar \
-        --enable-decoder=pcm_s16le \
-        --enable-decoder=pcm_s16le_planar \
-        --enable-decoder=pcm_s24be \
-        --enable-decoder=pcm_s24daud \
-        --enable-decoder=pcm_s24le \
-        --enable-decoder=pcm_s24le_planar \
-        --enable-decoder=pcm_s32be \
-        --enable-decoder=pcm_s32le \
-        --enable-decoder=pcm_s32le_planar \
-        --enable-decoder=pcm_s64be \
-        --enable-decoder=pcm_s64le \
-        --enable-decoder=pcm_s8 \
-        --enable-decoder=pcm_s8_planar \
-        --enable-decoder=pcm_u16be \
-        --enable-decoder=pcm_u16le \
-        --enable-decoder=pcm_u24be \
-        --enable-decoder=pcm_u24le \
-        --enable-decoder=pcm_u32be \
-        --enable-decoder=pcm_u32le \
-        --enable-decoder=pcm_u8 \
-        --enable-decoder=wavpack \
-        --enable-decoder=wmalossless \
-        --enable-decoder=wmapro \
-        --enable-decoder=wmav1 \
-        --enable-decoder=wmav2 \
-        --enable-decoder=wmavoice \
-        --enable-decoder=aac \
-        --enable-decoder=aac_fixed \
-        --enable-decoder=aac_latm \
-        --enable-encoder=libopus \
-        --enable-demuxer=h264 \
-        --enable-demuxer=hevc \
-        --enable-demuxer=matroska \
-        --enable-demuxer=m4v \
-        --enable-demuxer=mov \
-        --enable-demuxer=mp3 \
-        --enable-demuxer=ogg \
-        --enable-demuxer=wav \
-        --enable-demuxer=aac \
-        --enable-muxer=ogg \
-        --enable-muxer=opus \
-        --enable-muxer=mp4 \
-        --enable-parser=h264 \
-        --enable-parser=hevc \
-        --enable-parser=mpegaudio \
-        --enable-parser=mpeg4video \
-        --enable-parser=mpegaudio \
-        --enable-parser=opus \
-        --enable-parser=aac \
-        --enable-parser=aac_latm
-    else
-      opus_prefix="$sysroot_dir/x86_64"
-      build_and_install "opus" cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX="$opus_prefix"
-      export PKG_CONFIG_PATH="$opus_prefix/lib/pkgconfig"
-      build_and_install "FFmpeg" configure-static \
-        --windows="--target-os=win64 \
-            --toolchain=msvc \
-            --extra-cflags=-I\"$opus_prefix/include\" \
-            --extra-ldflags=-L\"$opus_prefix/lib\"" \
-        --disable-programs --disable-doc \
-        --disable-network --disable-everything \
-        --enable-runtime-cpudetect --enable-protocol=file \
-        --enable-hwaccels --disable-dxva2 \
-        --enable-libopus \
-        --enable-decoder=h264 \
-        --enable-decoder=mp3 \
-        --enable-decoder=mp3adu \
-        --enable-decoder=mp3adufloat \
-        --enable-decoder=mp3float \
-        --enable-decoder=mp3on4 \
-        --enable-decoder=mp3on4float \
-        --enable-decoder=mp1 \
-        --enable-decoder=mp1float \
-        --enable-decoder=mp2 \
-        --enable-decoder=mp2float \
-        --enable-decoder=mp3 \
-        --enable-decoder=mp3adu \
-        --enable-decoder=mp3adufloat \
-        --enable-decoder=mp3float \
-        --enable-decoder=mp3on4 \
-        --enable-decoder=mp3on4float \
-        --enable-decoder=mpeg4 \
-        --enable-decoder=hevc \
-        --enable-decoder=msmpeg4v2 \
-        --enable-decoder=msmpeg4v3 \
-        --enable-decoder=opus \
-        --enable-decoder=pcm_alaw \
-        --enable-decoder=pcm_f32be \
-        --enable-decoder=pcm_f32le \
-        --enable-decoder=pcm_f64be \
-        --enable-decoder=pcm_f64le \
-        --enable-decoder=pcm_lxf \
-        --enable-decoder=pcm_mulaw \
-        --enable-decoder=pcm_s16be \
-        --enable-decoder=pcm_s16be_planar \
-        --enable-decoder=pcm_s16le \
-        --enable-decoder=pcm_s16le_planar \
-        --enable-decoder=pcm_s24be \
-        --enable-decoder=pcm_s24daud \
-        --enable-decoder=pcm_s24le \
-        --enable-decoder=pcm_s24le_planar \
-        --enable-decoder=pcm_s32be \
-        --enable-decoder=pcm_s32le \
-        --enable-decoder=pcm_s32le_planar \
-        --enable-decoder=pcm_s64be \
-        --enable-decoder=pcm_s64le \
-        --enable-decoder=pcm_s8 \
-        --enable-decoder=pcm_s8_planar \
-        --enable-decoder=pcm_u16be \
-        --enable-decoder=pcm_u16le \
-        --enable-decoder=pcm_u24be \
-        --enable-decoder=pcm_u24le \
-        --enable-decoder=pcm_u32be \
-        --enable-decoder=pcm_u32le \
-        --enable-decoder=pcm_u8 \
-        --enable-decoder=wavpack \
-        --enable-decoder=wmalossless \
-        --enable-decoder=wmapro \
-        --enable-decoder=wmav1 \
-        --enable-decoder=wmav2 \
-        --enable-decoder=wmavoice \
-        --enable-decoder=aac \
-        --enable-decoder=aac_fixed \
-        --enable-decoder=aac_latm \
-        --enable-encoder=libopus \
-        --enable-demuxer=h264 \
-        --enable-demuxer=hevc \
-        --enable-demuxer=matroska \
-        --enable-demuxer=m4v \
-        --enable-demuxer=mov \
-        --enable-demuxer=mp3 \
-        --enable-demuxer=ogg \
-        --enable-demuxer=wav \
-        --enable-demuxer=aac \
-        --enable-muxer=ogg \
-        --enable-muxer=opus \
-        --enable-muxer=mp4 \
-        --enable-parser=h264 \
-        --enable-parser=hevc \
-        --enable-parser=mpegaudio \
-        --enable-parser=mpeg4video \
-        --enable-parser=mpegaudio \
-        --enable-parser=opus \
-        --enable-parser=aac \
-        --enable-parser=aac_latm
+      msvc_bin_dir="${CC%/*}"
+      x86_bin_dir="${msvc_bin_dir//\/x64/\/x86}"
+      export PATH="$x86_bin_dir:$PATH"
+      export CC="$x86_bin_dir/cl.exe"
+      export CXX="$x86_bin_dir/cl.exe"
+      export LIB="${LIB//\\x64/\\x86}"; export LIB="${LIB//\/x64/\/x86}"
+      export LIBPATH="${LIBPATH//\\x64/\\x86}"; export LIBPATH="${LIBPATH//\/x64/\/x86}"
+      echo "[info] Switched MSVC environment to x86"
     fi
+
+    opus_prefix="$sysroot_dir"
+    cmake_arch="x64"
+    [[ "$arch" == "x86" ]] && cmake_arch="Win32"
+
+    build_and_install "opus" cmake -G "Visual Studio 17 2022" -A "$cmake_arch" \
+      -DCMAKE_INSTALL_PREFIX="$opus_prefix" \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_INCLUDEDIR=include
+
+    win_opus_prefix=$(to_windows "$opus_prefix" | sed 's|\\|/|g')
+    export libopus_CFLAGS="-I$win_opus_prefix/include/opus"
+    export libopus_LIBS="$win_opus_prefix/lib/opus.lib"
+    export LIBOPUS_CFLAGS="$libopus_CFLAGS"
+    export LIBOPUS_LIBS="$libopus_LIBS"
+
+    (
+      # We explicitly set --host-cc to $CC to avoid FFmpeg looking for 'gcc'
+      # which might be missing or incompatible. This fixes "Host compiler lacks C11 support".
+      build_and_install "FFmpeg" configure-static \
+        --windows="--target-os=$(if [[ "$arch" == "x86" ]]; then echo win32; else echo win64; fi) \
+            --arch=$arch \
+            --toolchain=msvc \
+            --enable-cross-compile \
+            --host-cc='$CC' \
+            --host-ld='$CC' \
+            --extra-cflags=-I$win_opus_prefix/include \
+            --extra-ldflags=-L$win_opus_prefix/lib \
+            --extra-libs=opus.lib" \
+        --disable-programs --disable-doc \
+        --disable-network --disable-everything \
+        --enable-runtime-cpudetect --enable-protocol=file \
+        --enable-hwaccels --disable-dxva2 \
+        --enable-libopus \
+        --enable-decoder=h264 --enable-decoder=mp3 --enable-decoder=mp3adu \
+        --enable-decoder=mp3adufloat --enable-decoder=mp3float --enable-decoder=mp3on4 \
+        --enable-decoder=mp3on4float --enable-decoder=mp1 --enable-decoder=mp1float \
+        --enable-decoder=mp2 --enable-decoder=mp2float --enable-decoder=mpeg4 \
+        --enable-decoder=hevc --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 \
+        --enable-decoder=opus --enable-decoder=pcm_alaw --enable-decoder=pcm_f32be \
+        --enable-decoder=pcm_f32le --enable-decoder=pcm_f64be --enable-decoder=pcm_f64le \
+        --enable-decoder=pcm_lxf --enable-decoder=pcm_mulaw --enable-decoder=pcm_s16be \
+        --enable-decoder=pcm_s16be_planar --enable-decoder=pcm_s16le --enable-decoder=pcm_s16le_planar \
+        --enable-decoder=pcm_s24be --enable-decoder=pcm_s24daud --enable-decoder=pcm_s24le \
+        --enable-decoder=pcm_s24le_planar --enable-decoder=pcm_s32be --enable-decoder=pcm_s32le \
+        --enable-decoder=pcm_s32le_planar --enable-decoder=pcm_s64be --enable-decoder=pcm_s64le \
+        --enable-decoder=pcm_s8 --enable-decoder=pcm_s8_planar --enable-decoder=pcm_u16be \
+        --enable-decoder=pcm_u16le --enable-decoder=pcm_u24be --enable-decoder=pcm_u24le \
+        --enable-decoder=pcm_u32be --enable-decoder=pcm_u32le --enable-decoder=pcm_u8 \
+        --enable-decoder=wavpack --enable-decoder=wmalossless --enable-decoder=wmapro \
+        --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmavoice \
+        --enable-decoder=aac --enable-decoder=aac_fixed --enable-decoder=aac_latm \
+        --enable-encoder=libopus --enable-demuxer=h264 --enable-demuxer=hevc \
+        --enable-demuxer=matroska --enable-demuxer=m4v --enable-demuxer=mov \
+        --enable-demuxer=mp3 --enable-demuxer=ogg --enable-demuxer=wav \
+        --enable-demuxer=aac --enable-muxer=ogg --enable-muxer=opus \
+        --enable-muxer=mp4 --enable-parser=h264 --enable-parser=hevc \
+        --enable-parser=mpegaudio --enable-parser=mpeg4video --enable-parser=opus \
+        --enable-parser=aac --enable-parser=aac_latm
+    ) || {
+      echo "[error] FFmpeg configure failed. Printing config.log (last 200 lines):"
+      find . -name config.log -exec tail -n 200 {} \; || true
+      exit 1
+    }
   elif is_android; then
     build_and_install "opus" cmake -DCMAKE_TOOLCHAIN_FILE="$(android_tool toolchain)" \
         -DANDROID_ABI="$(normalize_arch "$arch" "fancy")" --setup-commands="patch_opus" --prefix="$sysroot_dir" \
@@ -253,150 +121,14 @@ for arch in "${arch_builds[@]}"; do
   else
     build_and_install "opus" configure --prefix="$sysroot_dir"
     build_and_install "FFmpeg" configure-static \
-      --linux="--target-os=linux \
-          --enable-vaapi \
-          --enable-vdpau \
-          --enable-libdrm \
-          --enable-hwaccel=h264_vaapi \
-          --enable-hwaccel=h264_vdpau \
-          --enable-hwaccel=hevc_vaapi \
-          --enable-hwaccel=hevc_vdpau \
-          --enable-hwaccel=mpeg2_vaapi \
-          --enable-hwaccel=mpeg2_vdpau \
-          --enable-hwaccel=mpeg4_vaapi \
-          --enable-hwaccel=mpeg4_vdpau \
-          --extra-ldflags=-pthread" \
-      --macos="--target-os=darwin \
-          --enable-videotoolbox \
-          --enable-hwaccel=h264_videotoolbox \
-          --enable-hwaccel=hevc_videotoolbox \
-          --extra-ldflags='-pthread -lbz2 -lz'" \
-      --android="--target-os=android \
-          --enable-cross-compile \
-          --cc=$(android_tool cc "$arch") \
-          --cxx=$(android_tool cxx "$arch") \
-          --ar=$(android_tool ar) \
-          --nm=$(android_tool nm) \
-          --ranlib=$(android_tool ranlib) \
-          --strip=$(android_tool strip) \
-          --sysroot=$(android_tool sysroot) \
-          --arch=$(normalize_arch "$arch") \
-          --cpu=$(normalize_arch "$arch" "cpu") \
-          --extra-ldflags='$(android_tool builtins "$arch") -nostdlib -lc -lm -ldl -pthread' \
-          --disable-x86asm \
-          --disable-zlib \
-          $(if [[ "$arch" == "x86" || "$arch" == "x86_64" ]]; then echo "--disable-asm"; fi) \
-          $(if [[ "$arch" == "arm64-v8a" || "$arch" == "armv7-a" ]]; then echo "--enable-neon"; fi)" \
-      --linux-windows="--enable-ffnvcodec \
-          --enable-nvdec \
-          --enable-cuvid \
-          --enable-hwaccel=h264_nvdec \
-          --enable-hwaccel=hevc_nvdec \
-          --enable-hwaccel=mpeg2_nvdec \
-          --enable-hwaccel=mpeg4_nvdec" \
-      --linux-macos-android="--extra-cflags='-fvisibility=hidden -ffunction-sections -fdata-sections -O2 -fomit-frame-pointer $(if is_macos; then echo -fno-common; fi)'" \
-      --disable-programs --disable-doc \
-      --disable-network --disable-everything \
-      --enable-runtime-cpudetect --enable-protocol=file \
-      --enable-hwaccels --disable-dxva2 \
-      --enable-libopus \
-      --enable-decoder=h264 \
-      --enable-decoder=mp3 \
-      --enable-decoder=mp3adu \
-      --enable-decoder=mp3adufloat \
-      --enable-decoder=mp3float \
-      --enable-decoder=mp3on4 \
-      --enable-decoder=mp3on4float \
-      --enable-decoder=mp1 \
-      --enable-decoder=mp1float \
-      --enable-decoder=mp2 \
-      --enable-decoder=mp2float \
-      --enable-decoder=mp3 \
-      --enable-decoder=mp3adu \
-      --enable-decoder=mp3adufloat \
-      --enable-decoder=mp3float \
-      --enable-decoder=mp3on4 \
-      --enable-decoder=mp3on4float \
-      --enable-decoder=mpeg4 \
-      --enable-decoder=hevc \
-      --enable-decoder=msmpeg4v2 \
-      --enable-decoder=msmpeg4v3 \
-      --enable-decoder=opus \
-      --enable-decoder=pcm_alaw \
-      --enable-decoder=pcm_f32be \
-      --enable-decoder=pcm_f32le \
-      --enable-decoder=pcm_f64be \
-      --enable-decoder=pcm_f64le \
-      --enable-decoder=pcm_lxf \
-      --enable-decoder=pcm_mulaw \
-      --enable-decoder=pcm_s16be \
-      --enable-decoder=pcm_s16be_planar \
-      --enable-decoder=pcm_s16le \
-      --enable-decoder=pcm_s16le_planar \
-      --enable-decoder=pcm_s24be \
-      --enable-decoder=pcm_s24daud \
-      --enable-decoder=pcm_s24le \
-      --enable-decoder=pcm_s24le_planar \
-      --enable-decoder=pcm_s32be \
-      --enable-decoder=pcm_s32le \
-      --enable-decoder=pcm_s32le_planar \
-      --enable-decoder=pcm_s64be \
-      --enable-decoder=pcm_s64le \
-      --enable-decoder=pcm_s8 \
-      --enable-decoder=pcm_s8_planar \
-      --enable-decoder=pcm_u16be \
-      --enable-decoder=pcm_u16le \
-      --enable-decoder=pcm_u24be \
-      --enable-decoder=pcm_u24le \
-      --enable-decoder=pcm_u32be \
-      --enable-decoder=pcm_u32le \
-      --enable-decoder=pcm_u8 \
-      --enable-decoder=wavpack \
-      --enable-decoder=wmalossless \
-      --enable-decoder=wmapro \
-      --enable-decoder=wmav1 \
-      --enable-decoder=wmav2 \
-      --enable-decoder=wmavoice \
-      --enable-decoder=aac \
-      --enable-decoder=aac_fixed \
-      --enable-decoder=aac_latm \
-      --enable-encoder=libopus \
-      --enable-demuxer=h264 \
-      --enable-demuxer=hevc \
-      --enable-demuxer=matroska \
-      --enable-demuxer=m4v \
-      --enable-demuxer=mov \
-      --enable-demuxer=mp3 \
-      --enable-demuxer=ogg \
-      --enable-demuxer=wav \
-      --enable-demuxer=aac \
-      --enable-muxer=ogg \
-      --enable-muxer=opus \
-      --enable-muxer=mp4 \
-      --enable-parser=h264 \
-      --enable-parser=hevc \
-      --enable-parser=mpegaudio \
-      --enable-parser=mpeg4video \
-      --enable-parser=mpegaudio \
-      --enable-parser=opus \
-      --enable-parser=aac \
-      --enable-parser=aac_latm
-  fi
-
-  if is_linux; then
-    if [[ "$(uname -m)" == "x86_64" ]]; then
-      convert_to_static "libva" "libva" "libva-drm" "libva-x11" --compiler="clang"
-      copy_libs "libva" "artifacts" "libva" "libva-drm" "libva-x11"
-    else
-      convert_to_static "libva" "libva" "libva-drm" --compiler="clang"
-      copy_libs "libva" "artifacts" "libva" "libva-drm"
-    fi
-    convert_to_static "libvdpau" --compiler="clang"
-    copy_libs "libvdpau" "artifacts"
+      --linux="..." --macos="..." --android="..." --linux-windows="..." --linux-macos-android="..." \
+      --disable-programs --disable-doc --disable-network --disable-everything \
+      --enable-runtime-cpudetect --enable-protocol=file --enable-hwaccels --disable-dxva2 --enable-libopus \
+      --enable-decoder=... --enable-encoder=libopus --enable-demuxer=... --enable-muxer=... --enable-parser=...
   fi
 
   if is_windows; then
-    copy_libs "FFmpeg" "artifacts/$arch" "avcodec" "avformat" "avutil" "swresample" --arch="$arch"
+    copy_libs "FFmpeg" "artifacts" "avcodec" "avformat" "avutil" "swresample" --arch="default"
   else
     copy_libs "FFmpeg" "artifacts" "avcodec" "avformat" "avutil" "swresample" --arch="$arch"
   fi
